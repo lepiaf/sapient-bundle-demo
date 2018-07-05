3698f2d24ad10deb236c94df8434f82846bb19dd
Thierry Thuon <thierry@thuon.eu>
2018-07-05 13:50:18 +0200

    Update demo with seal/unseal request

--- api-alpha/config/bundles.php
+++ api-alpha/config/bundles.php
@@ -3,5 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-    lepiaf\SapientBundle\SapientBundle::class => ['prod', 'dev' => true],
+    lepiaf\SapientBundle\SapientBundle::class => ['0' => true, 'dev' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
--- api-beta/config/bundles.php
+++ api-beta/config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     lepiaf\SapientBundle\SapientBundle::class => ['all' => true],
     Csa\Bundle\GuzzleBundle\CsaGuzzleBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
