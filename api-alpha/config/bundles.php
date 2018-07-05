2d03fd35e5ca31d4b28520de158a6562a93e4b8a
Thierry Thuon <1940947+lepiaf@users.noreply.github.com>
2018-07-05 14:30:43 +0200

    Update demo with seal/unseal request (#2)
    * Update demo with seal/unseal request

--- api-alpha/config/bundles.php
+++ api-alpha/config/bundles.php
@@ -3,5 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-    lepiaf\SapientBundle\SapientBundle::class => ['prod', 'dev' => true],
+    lepiaf\SapientBundle\SapientBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
