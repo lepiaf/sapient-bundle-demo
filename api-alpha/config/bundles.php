7688f73ac21ac182bf1f7e88f6cf307024ee63bc
Thierry Thuon <thierry@thuon.eu>
2018-07-05 13:56:29 +0200

    fix typo

--- api-alpha/config/bundles.php
+++ api-alpha/config/bundles.php
@@ -3,6 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-    lepiaf\SapientBundle\SapientBundle::class => ['0' => true, 'dev' => true],
+    lepiaf\SapientBundle\SapientBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
