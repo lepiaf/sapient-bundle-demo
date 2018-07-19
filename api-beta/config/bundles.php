6605f524fa7d58b7f644b4e08b048c98e6736dec
Thierry Thuon <thierry@thuon.eu>
2018-07-19 19:12:44 +0200

    Use recipe to install sapient-bundle and bump to 0.2

--- api-beta/config/bundles.php
+++ api-beta/config/bundles.php
@@ -2,7 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    lepiaf\SapientBundle\SapientBundle::class => ['all' => true],
     Csa\Bundle\GuzzleBundle\CsaGuzzleBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    lepiaf\SapientBundle\SapientBundle::class => ['all' => true],
 ];
